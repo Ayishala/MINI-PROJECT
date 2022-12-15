09afc48e2f9b639f76d51857ba6b956209a1c06f
Ayishala <114227839+Ayishala@users.noreply.github.com>
2022-12-15 16:59:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 import streamlit as st
 import joblib
-model_nb = joblib.load('POEM-CLASSIFICATION')
-st.title('POEM CLASSIFICATION') #creates a title in web app
-ip = st.text_input('Enter POEM:') #creates a text box in web app
+model_nb = joblib.load('stress detection')
+st.title('stress detection') #creates a title in web app
+ip = st.text_input('Enter text:') #creates a text box in web app
 op = model_nb.predict([ip])
 if st.button('Predict'):
   st.title(op[0])
